b8dd2e81b8ac15a414f84e6db7ff8b2be1c30d2c
vinaybommana <vinaybommana7@gmail.com>
2018-02-19 20:46:29 +0530

    added non numeric and numeric functionality to the names and phone number

--- way2sms_trial_one.py
+++ way2sms_trial_one.py
@@ -1,6 +1,7 @@
 import way2sms
 import os
 
+# event_id dictionary
 event_ids = {
     '1': "Code N Code",
     '2': "Ensemble",
@@ -12,24 +13,40 @@ event_ids = {
     '8': "Paper Presentation"
 }
 
-login_ids = ["7660993932-766099",
-             "8332098992-83320"]
+# login ids for way2sms
+login_ids = [
+    "8332098992-83320",
+    "7660993932-766099",
+    "8919381090-891938"
+]
 
 
 def send_message(id, phone_number, Message):
     details = login_ids[id].split("-")
     q = way2sms.sms(details[0], details[1])
     q.send(phone_number, Message)
+    return q
 
 
 def enter_details():
     RFID_code = str(input("Enter the code: "))
+
     first_name = str(input("Enter first name: "))
     last_name = str(input("Enter last name: "))
-
     college = str(input("Enter college: "))
+
+    # ensuring the entered names are not numbers
+    while first_name.isnumeric() or last_name.isnumeric() or college.isnumeric():
+        print("Entered credentials are numeric, please enter name and college\n")
+        first_name = str(input("Enter first name: "))
+        last_name = str(input("Enter last name: "))
+        college = str(input("Enter college: "))
+
     phone_number = str(input("Enter phone number: "))
-    while len(phone_number) != 10:
+
+    # ensuring the entered phone number is a 10 digit string
+    while (len(phone_number) != 10) or not (phone_number.isnumeric()):
+        print("Please enter a valid phone number\n")
         phone_number = str(input("Enter phone number: "))
     email_id = str(input("Enter email: "))
 
@@ -91,17 +108,17 @@ if __name__ == "__main__":
             with open(str(events[2]) + ".csv", "a+") as three:
                 three.write(details[0] + "," + details[1] + " " + details[2] + "," + details[3] + "," + details[4] + "," + details[5] + "," + event_ids[events[2]] + "\n")
 
-            message ="Hi " + str(details[1]+details[2]) + "\n" + " You have been successfully registered for:\n" + str(event_ids[events[0]]) + "\n" + str(event_ids[events[1]]) + "\n" + str(event_ids[events[2]])
+            message = "Hi " + str(details[1] + " " + details[2]) + "\n" + " You have been successfully registered for:\n" + str(event_ids[events[0]]) + "\n" + str(event_ids[events[1]]) + "\n" + str(event_ids[events[2]])
             try:
                 if count > 96:
                     id += 1
                     count = 1
 
-                send_message(id, details[4], message)
+                if send_message(id, details[4], message):
+                    print("message sent successfully\n")
                 print(count)
                 count += 1
             except ConnectionError:
                 raise
         else:
             print("invalid response")
-
